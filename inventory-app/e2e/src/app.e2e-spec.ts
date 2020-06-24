754f767720592622c16ad8311cc483440af3f72f
TK Tran <tktrannn@live.com>
2020-06-24 18:07:42 -0400

    Began Protractor tests.

--- inventory-app/e2e/src/app.e2e-spec.ts
+++ inventory-app/e2e/src/app.e2e-spec.ts
@@ -1,23 +1,46 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+	let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+	beforeEach(() => {
+		page = new AppPage();
+	});
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('inventory-app app is running!');
-  });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+
+
+
+	// Tests elements
+	it('should display AppComponent title', () => {
+		page.navigateToHome();
+		expect(page.getTitleText()).toEqual('Admin View');
+	});
+
+	it('should display add-item title', () => {
+		page.navigateToAddItem();
+		expect(page.getAddItemTitleText()).toEqual('Add to Inventory');
+	});
+
+	// Tests routing
+	it('should route to add-item and compare title', () => {
+		page.navigateToHome();
+		page.routeToAddItem();
+
+		let x = element(by.css('.addItemTitle')).getText();
+		expect(page.getAddItemTitleText()).toEqual(x);
+	});
+
+
+
+
+
+	afterEach(async () => {
+		// Assert that there are no errors emitted from the browser
+		const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+		expect(logs).not.toContain(jasmine.objectContaining({
+			level: logging.Level.SEVERE,
+		} as logging.Entry));
+	});
 });
--- inventory-app/e2e/src/app.po.ts
+++ inventory-app/e2e/src/app.po.ts
@@ -1,13 +1,32 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
-  }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<
-      string
-    >;
-  }
+	// For protractor to navigate between our pages
+	navigateToHome(): Promise<unknown> {
+		return browser.get(browser.baseUrl) as Promise<unknown>;
+	}
+
+	navigateToAddItem(): Promise<unknown> {
+		return browser.get(browser.addItemUrl) as Promise<unknown>;
+	}
+
+	navigateToInventoryList(): Promise<unknown> {
+		return browser.get(browser.inventoryListUrl) as Promise<unknown>;
+	}
+
+	// Tests elements
+	getTitleText(): Promise<string> {
+		return element(by.css('.navbar-brand')).getText() as Promise<string>;
+	}
+
+	getAddItemTitleText(): Promise<string> {
+		return element(by.tagName('p')).getText() as Promise<string>;
+	}
+
+	// Tests routing
+	routeToAddItem(): Promise<any> {
+		return element(by.css('[routerLink="/add-item"]')).click() as Promise<any>;
+	}
+
 }
