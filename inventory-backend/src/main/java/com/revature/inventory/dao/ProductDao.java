252a74566fa2c19448dbd1b1e8178f00db608539
rfenix3 <richard.fenix@gmail.com>
2020-06-25 11:43:41 -0400

    Add DAO testing using JUnit.

--- inventory-backend/src/main/java/com/revature/inventory/dao/ProductDao.java
+++ inventory-backend/src/main/java/com/revature/inventory/dao/ProductDao.java
@@ -1,10 +1,15 @@
 package com.revature.inventory.dao;
 
 import com.revature.inventory.model.Product;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductDao extends JpaRepository<Product, Long> {
+	
+	List<Product> findByCategory(String category);
 
 }
